1850628d45eaed7f40d88253bb37c55926d3e23b
Coinmemes <coinmemesy@gmail.com>
2025-02-17 21:15:05 +0100

    Update image-converter.js

--- image-converter.js
+++ image-converter.js
@@ -125,7 +125,7 @@ function resetImage() {
 function downloadImage() {
     const canvas = document.getElementById('imageCanvas');
     const link = document.createElement('a');
-    link.download = 'tinyurl.com/2ft5aap6.png';
+    link.download = 'ca_on_Solana_9bDTN4FahNPaQLTk9p6rjjbL3Uej46CCcAwyuKxFpump.png';
     link.href = canvas.toDataURL();
     link.click();
 }
